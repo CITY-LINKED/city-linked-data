876679438f113d641c604d49a90c9fe05131ee0c
CITY-LINKED <dras_00@hotmail.com>
2025-06-01 19:54:11 +0300

    Add files via upload

--- vote.py
+++ vote.py
@@ -1,5 +1,5 @@
-
 import json
+import subprocess
 from datetime import datetime
 
 # Load vote request
@@ -33,6 +33,13 @@ log_entry = f"{timestamp} — {user} voted for {city}\n"
 with open("Voting_Log.txt", "a") as log:
     log.write(log_entry)
 
-# Reset vote_request.json
+# Clear vote request
 with open("vote_request.json", "w") as f:
     json.dump({}, f)
+
+# Git commit and push changes
+subprocess.run(["git", "config", "user.name", "github-actions"])
+subprocess.run(["git", "config", "user.email", "github-actions@github.com"])
+subprocess.run(["git", "add", "votes.json", "Voting_Log.txt", "vote_request.json"])
+subprocess.run(["git", "commit", "-m", f"Register vote for {city}"])
+subprocess.run(["git", "push"])
